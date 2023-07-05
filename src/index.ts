c81c194c0f23c63176ddc490aeddfa3be2c8ac1b
Abdellah-SLIMANI <morscor21@gmail.com>
2023-07-05 23:31:56 +0200

    setup of the exporess and apollo server, and a crud operations on the posts

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,30 @@
 import { MikroORM } from "@mikro-orm/core";
-import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
+import { PORT, __prod__ } from "./constants";
 import microConfigs from "./mikro-orm.config"
+import express from "express"
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/Hello";
+import { PostResolver } from "./resolvers/Post";
 
 const main = async () => {
     const orm = await MikroORM.init(microConfigs);
     await orm.getMigrator().up();
-    const post = orm.em.create(Post, {
-        title: 'First Post',
-        createdAt: new Date(),
-        updatedAt:  new Date(),
-    });
-    // orm.em.persistAndFlush(post);
 
-    const posts = await orm.em.find(Post, {})
-    console.log({posts})
+    const app = express();
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver, PostResolver],
+            validate: false
+        }),
+        context: () => ({em: orm.em})
+    })
+    await apolloServer.start();
+    apolloServer.applyMiddleware({app})
+    app.listen(PORT, () => {
+        console.log(`Server is running on ${PORT}`)
+    })
 }
 
 main().catch(err => {
